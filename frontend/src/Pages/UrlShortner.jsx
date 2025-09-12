4f9bf10f5cfa78f8dc8856c78a270971b5351832
VishnuvardhanNekkanti <vishnunekkanti.05@gmail.com>
2025-09-12 10:10:25 +0530

    formatted

--- frontend/src/Pages/UrlShortner.jsx
+++ frontend/src/Pages/UrlShortner.jsx
@@ -1,37 +1,46 @@
-import { Stack, TextInput } from '@mantine/core'
-import React from 'react'
+import { Stack, TextInput } from "@mantine/core";
+import React, { useState } from "react";
 
 const UrlShortner = () => {
+  const [originalUrl, setOriginalUrl] = useState();
+  const [customLink, setCustomLink] = useState();
+  const [title, setTitle] = useState();
+  const [expiry, setExpiry] = useState();
+
   return (
     <div>
       <Stack>
-        <Text size ='30px'>Shorten Url Here</Text>
-            <TextInput
-    label="Original Url"
-    withAsterisk
-    onChange={(e) => setOriginalUrl(e.target.value)}
-    value={originalUrl}
-    />
-    <TextInput
-label="Customize Your Url (Optional)"
-withAsterisk
-onChange={(e) => setCustomLink(e.target.value)}
-value={customLink}
-/>
-<TextInput
-label="Title (Optional)"
-withAsterisk
-onChange={(e) => setTitle(e. target. value) } />
-<TextInput
-label="Date of expiry (optional)"
-onChange={(e)=> setExpiry(e.target.value)}
-value={expiry}
-type='date'
-/>
-<Button variant='outline' disabled={!originalUrl}>Generate short url</Button>
+        <Text size="30px">Shorten Url Here</Text>
+        <TextInput
+          label="Original Url"
+          withAsterisk
+          onChange={(e) => setOriginalUrl(e.target.value)}
+          value={originalUrl}
+        />
+        <TextInput
+          label="Customize Your Url (Optional)"
+          withAsterisk
+          onChange={(e) => setCustomLink(e.target.value)}
+          value={customLink}
+        />
+        <TextInput
+          label="Title (Optional)"
+          withAsterisk
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <TextInput
+          label="Date of expiry (optional)"
+          onChange={(e) => setExpiry(e.target.value)}
+          value={expiry}
+          type="date"
+        />
+        <Button variant="outline" disabled={!originalUrl}>
+          Generate short url
+        </Button>
       </Stack>
     </div>
-  )
-}
+  );
+};
 
-export default UrlShortner
+export default UrlShortner;
